74f2bcb71ba368aab33421d3899304bac3008af4
Ajri Sylhasi <ajrisylhasi@gmail.com>
2025-09-10 23:32:49 +0200

    js compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = nil
+  config.assets.js_compressor = nil
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
